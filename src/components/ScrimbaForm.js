fe61442d6da6cde21c783b8cbfc79edf12f41050
David Hunter <david@davidhunter.io>
2021-04-20 13:21:03 -0700

    init commit, form css is finished

---
+++ src/components/ScrimbaForm.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+export default function ScrimbaForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("** Submited **");
+  };
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="inputs"
+          placeholder="name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          aria-label="name"
+          required
+        />
+        <input
+          type="email"
+          className="inputs"
+          placeholder="email"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          aria-label="email"
+          required
+        />
+        <button className="inputs" type="submit" id="theButton">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+}
